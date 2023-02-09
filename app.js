729cbb588be4141dee31c2198f295c21bb024777
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-02-09 22:04:07 +0100

    home

---
+++ app.js
@@ -0,0 +1,61 @@
+/*
+Require express function
+*/
+
+const express = require("express");
+const hbs = require("hbs");
+
+/*
+Create the app object
+*/
+
+const app = express()
+app.set('view engine', 'hbs')
+
+app.use(express.static(`${__dirname}/public`))
+hbs.registerPartials(`${__dirname}/views/partials`);
+
+
+app.get('/',(request, response) =>{
+    response.render('home', {
+        navbar: true,
+        footer:true,
+        title:"Roberto | Home"
+    })
+})
+
+app.get('/about',(request, response) =>{
+    response.render('about', {
+        navbar: true,
+        footer:true,
+        title:"Roberto | About"
+    })
+})
+
+app.get('/work',(request, response) =>{
+    response.render('work', {
+        navbar: true,
+        footer:true,
+        title:"Roberto | Work"
+    })
+})
+
+app.get('/media',(request, response) =>{
+    response.render('media', {
+        navbar: true,
+        footer:true,
+        title:"Roberto | Media"
+    })
+})
+
+
+
+/*
+Listen to a specific port
+*/
+
+app.listen(3000,()=>{
+console.log('🏃🏻‍♀️ on http://localhost:3000')
+})
+
+
