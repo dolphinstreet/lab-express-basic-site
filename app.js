d8b7ac0684494fbdb09955130324e169b05bb53d
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-02-14 11:43:56 +0100

    home and about

--- app.js
+++ app.js
@@ -16,46 +16,44 @@ app.use(express.static(`${__dirname}/public`))
 hbs.registerPartials(`${__dirname}/views/partials`);
 
 
-app.get('/',(request, response) =>{
+app.get('/', (request, response) => {
+    console.log("ciao")
     response.render('home', {
         navbar: true,
-        footer:true,
-        title:"Roberto | Home"
+        footer: true,
+        title: "Roberto | Home"
     })
 })
 
-app.get('/about',(request, response) =>{
+app.get('/about', (request, response) => {
     response.render('about', {
         navbar: true,
-        footer:true,
-        title:"Roberto | About"
+        footer: true,
+        title: "Roberto | About"
     })
 })
 
-app.get('/work',(request, response) =>{
+app.get('/work', (request, response) => {
     response.render('work', {
         navbar: true,
-        footer:true,
-        title:"Roberto | Work"
+        footer: true,
+        title: "Roberto | Work"
     })
 })
 
-app.get('/media',(request, response) =>{
+app.get('/media', (request, response) => {
     response.render('media', {
         navbar: true,
-        footer:true,
-        title:"Roberto | Media"
+        footer: true,
+        title: "Roberto | Media"
     })
 })
-
 
 
 /*
 Listen to a specific port
 */
 
-app.listen(3000,()=>{
-console.log('🏃🏻‍♀️ on http://localhost:3000')
+app.listen(3000, () => {
+    console.log('🏃🏻‍♀️ on http://localhost:3000')
 })
-
-
